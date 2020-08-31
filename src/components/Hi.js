a376e2150b1db338ee9af4de47bc34334d67c83f
Mateo Clarke <code@mateoclarke.com>
2020-08-31 10:51:12 -0400

    testing auto deployment gh-action

--- src/components/Hi.js
+++ src/components/Hi.js
@@ -1,13 +1,11 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 export default function Hi({ data }) {
-  console.log(data);
   return (
     <h1
       style={{
         backgroundColor: data.state === "DARK" ? "black" : "white",
-        color: data.state === "DARK" ? "white" : "black",
+        color: data.state === "DARK" ? "gray" : "black",
       }}
     >
       Hi
