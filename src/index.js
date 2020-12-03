83594a0a22bafd278ca5a9345fb143f5a92ab4ab
Sergio Garcia <sergiogcx@gmail.com>
2020-12-03 14:13:20 -0600

    Export GQLAbstract class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import styles from "./styles.module.css";
+import GQLAbstract from "./graphql/GQLAbstract";
 
 import { Hi } from "./components/Hi";
-export { Hi };
+export { Hi, GQLAbstract };
 
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>;
